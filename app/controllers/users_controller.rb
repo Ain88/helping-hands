3592202a8460d68bd260491bf379c50a18849124
ain88 <ahyungss@hotmail.com>
2020-07-21 19:25:28 -0700

    clean up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,58 @@
 class UsersController < ApplicationController
-  def index
-     @users = User.all
-     render json: @users
- end
+ #  def index
+ #     @users = User.all
+ #     render json: @users
+ # end
+ #
+ # def create
+ #     @user = User.create(user_params)
+ # end
 
- def create
-     @user = User.create(user_params)
- end
-#
-# def index
-#     @users = User.all
-#
-#     if @users
-#       render json: {
-#         users: @users
-#       }
-#     else
-#       render json: {
-#         status: 500,
-#         errors: ['no users found'],
-#       }
-#     end
-# end
-#
-# def show
-#     @user = User.find(params[:id])
-#    if @user
-#       render json: {
-#         user: @user
-#       }
-#     else
-#       render json: {
-#         status: 500,
-#         errors: ['user not found']
-#       }
-#     end
-#   end
-#
-#   def create
-#     @user = User.new(user_params)
-#
-#     if @user.save
-#       login!
-#       render json: {
-#         status: :created,
-#         user: @user
-#       }
-#     else
-#       render json: {
-#         status: 500,
-#         errors: @user.errors.full_messages
-#       }
-#     end
-#   end
+def index
+    @users = User.all
+
+    if @users
+      render json: {
+        users: @users
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['no users found'],
+      }
+    end
+end
+
+def show
+    @user = User.find(params[:id])
+   if @user
+      render json: {
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: ['user not found']
+      }
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else
+      render json: {
+        status: 500,
+        errors: @user.errors.full_messages
+      }
+    end
+  end
 
 private
 
