065970754c5300ae4f77f44a0feb9eb10502f157
ain88 <ahyungss@hotmail.com>
2020-11-01 19:07:57 -0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,20 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
   def login!
-    session[:user_id] = @user.id
+    cookies[:user_id] = @user.id
   end
   def logged_in?
-    !!session[:user_id]
+    !!cookies[:user_id]
   end
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(cookies[:user_id]) if cookies[:user_id]
     rescue ActiveRecord::RecordNotFound
   end
   def authorized_user?
     @user == current_user
   end
   def logout!
-    session.clear
+    cookies.clear
   end
 
 end
