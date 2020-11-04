ee376ce43b9d703453e02f68e9ebb97e6d98a3f7
ain88 <ahyungss@hotmail.com>
2020-11-03 22:47:11 -0800

    update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :redis_store, {
-    server: ENV['redis://redistogo:011f3ea2d5060d4b361a0d29fd297e3d@scat.redistogo.com:10473/'],
-    expire_after: 1.week
-    } 
+  Rails.application.config.session_store :cookie_store, key: '_HelpVan'
 else
   Rails.application.config.session_store :cookie_store, key: '_HelpVan'
 end
