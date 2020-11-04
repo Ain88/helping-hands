30c09770b6e2fb220d18390e70b551857c72b674
ain88 <ahyungss@hotmail.com>
2020-11-03 22:29:37 -0800

    update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,8 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_HelpVan', domain: 'helpvan-front.herokuapp.com'
+  Rails.application.config.session_store :redis_store, {
+    server: ENV['redis://redistogo:011f3ea2d5060d4b361a0d29fd297e3d@scat.redistogo.com:10473/'],
+    expire_after: 1.week
+    } 
 else
   Rails.application.config.session_store :cookie_store, key: '_HelpVan'
 end
