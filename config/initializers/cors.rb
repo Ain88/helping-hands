648b8a70d5663961d4012c192f7503f15bfd60fb
Ain88 <ahyungss@hotmail.com>
2021-07-03 20:04:19 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://help-van.herokuapp.com'
+    origins 'https://jolly-benz-c2004d.netlify.app'
 
     resource '*',
       headers: :any,
