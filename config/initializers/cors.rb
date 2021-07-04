cd99e6a25748de7b819b292518fa3ba815bc22c0
Ain88 <ahyungss@hotmail.com>
2021-07-03 20:02:31 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://helpvan-front.herokuapp.com'
+    origins 'https://help-van.herokuapp.com'
 
     resource '*',
       headers: :any,
